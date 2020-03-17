97e34ff86a25b1add895cf44e90d185da30e2f5d
Omri Gazitt <ogazitt@gmail.com>
2020-03-16 21:15:17 -1000

    improved form controls for parameters

--- src/components/SnapDefinition.js
+++ src/components/SnapDefinition.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { useApi } from '../utils/api'
 import { navigate } from 'hookrouter'
 import { useConnections } from '../utils/connections'
-import { Button, Tabs, Tab, Card, Form, Row, Col } from 'react-bootstrap'
+import { Button, Tabs, Tab, Card, Form, Row, Col, InputGroup, FormControl } from 'react-bootstrap'
 import Highlight from '../components/Highlight'
 import ProviderCard from '../components/ProviderCard'
 
@@ -40,71 +40,69 @@ const SnapDefinition = ({snap, activeSnap, activeSnapId}) => {
 
   return (
     <Tabs activeKey={key} onSelect={k => setKey(k)}>
-    <Tab eventKey="visual" title={<span><i className="fa fa-sitemap" />&nbsp;&nbsp;Visual</span>}>
-      <h5 style={{ margin: 10 }}>{snap && snap.description}</h5>
-      <div style={{ display: 'flex' }}>
-        { provider && <ProviderCard provider={provider} /> }
-        { provider && <i style={{ fontSize: '6em', margin: 50 }} className="fa fa-play text-muted" /> }
-        { actionList && actionList.map(a => { 
-            const actionProvider = connections && connections.find(el => el.provider === a);
-            return actionProvider && <ProviderCard key={a} provider={actionProvider} />
-          })
-        }
-      </div>
-      <h5 style={{ margin: 10 }}>{snap && 'Parameters:'}</h5>
-      <div style={{ marginLeft: 20 }}>
-        { snap && snap.parameters && 
-          <Card>
-            <Card.Body>
-            { 
-              activeSnapId ? 
-                activeSnap && activeSnap.params.map(p => 
-                  <div key={p.name} style={{ display: 'flex'}}>
-                    <h5 style={{ fontWeight: 400 }}>{p.name}: </h5>
-                    <h5>&nbsp;&nbsp;{p.value}</h5>
-                  </div>
-                ) :
-                snap.parameters.map(p => 
-                  <div key={p.name} style={{ display: 'flex'}}>
-                    <h5 style={{ fontWeight: 400 }}>{p.name}: </h5>
-                    <h5>&nbsp;&nbsp;{p.description}</h5>
-                  </div> 
-                )
-            }
-            </Card.Body>
-          </Card>
-        }
-      </div>
-    </Tab>
+      <Tab eventKey="visual" title={<span><i className="fa fa-sitemap" />&nbsp;&nbsp;Visual</span>}>
+        <h5 style={{ margin: 10 }}>{snap && snap.description}</h5>
+        <div style={{ display: 'flex' }}>
+          { provider && <ProviderCard provider={provider} /> }
+          { provider && <i style={{ fontSize: '6em', margin: 50 }} className="fa fa-play text-muted" /> }
+          { actionList && actionList.map(a => { 
+              const actionProvider = connections && connections.find(el => el.provider === a);
+              return actionProvider && <ProviderCard key={a} provider={actionProvider} />
+            })
+          }
+        </div>
+        <h5 style={{ margin: 10 }}>{snap && 'Parameters:'}</h5>
+        <div style={{ marginLeft: 20 }}>
+          { snap && snap.parameters && 
+            <Card>
+              <Card.Body>
+              { 
+                activeSnapId ? 
+                  activeSnap && activeSnap.params.map(p => 
+                    <div key={p.name} style={{ display: 'flex'}}>
+                      <h5 style={{ fontWeight: 400 }}>{p.name}: </h5>
+                      <h5>&nbsp;&nbsp;{p.value}</h5>
+                    </div>
+                  ) :
+                  snap.parameters.map(p => 
+                    <div key={p.name} style={{ display: 'flex'}}>
+                      <h5 style={{ fontWeight: 400 }}>{p.name}: </h5>
+                      <h5>&nbsp;&nbsp;{p.description}</h5>
+                    </div> 
+                  )
+              }
+              </Card.Body>
+            </Card>
+          }
+        </div>
+      </Tab>
 
-    <Tab eventKey="code" title={<span><i className="fa fa-code" />&nbsp;&nbsp;Code</span>}>
-      { snap && snap.text && <Highlight language='yaml'>{snap.text}</Highlight> }
-    </Tab>
+      <Tab eventKey="code" title={<span><i className="fa fa-code" />&nbsp;&nbsp;Code</span>}>
+        { snap && snap.text && <Highlight language='yaml'>{snap.text}</Highlight> }
+      </Tab>
 
-    { !activeSnapId && /* "Activate" tab not available for already active snaps */
-    <Tab eventKey="activate" title={<span><i className="fa fa-play" />&nbsp;&nbsp;Activate</span>}>
-      <Card style={{ minWidth: 'calc(100% - 220px)', maxWidth: 'calc(100% - 220px)'}}>
-      { 
-        snap && snap.parameters && snap.parameters.map(p => 
-        <Form key={p.name}>
-          <div>
-            <Form.Group as={Row} style={{ margin: 20 }}>
-              <Form.Label style={{ fontWeight: 400 }} column sm="2">{p.name}: </Form.Label>
-              <Col sm="10">
-                <Form.Control placeholder={p.description} onChange={ (e) => { p.value = e.target.value }} />
-              </Col>
-            </Form.Group>
-          </div>
-        </Form> 
-        )
+      { !activeSnapId && /* "Activate" tab not available for already active snaps */
+      <Tab eventKey="activate" title={<span><i className="fa fa-play" />&nbsp;&nbsp;Activate</span>}>
+        { snap && snap.parameters && snap.parameters.map(p => 
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+                <InputGroup.Text style={{ minWidth: 120 }} id={p.name}>{p.name}</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                aria-label="account"
+                aria-describedby={p.name}
+                placeholder={p.description} 
+                onChange={(e) => { p.value = e.target.value } }
+              />
+            </InputGroup>
+          )
+        }
+        <Button variant="primary" style={{ marginTop: 10 }} onClick={ activate }>
+          <i className="fa fa-play"></i>&nbsp;&nbsp;Activate
+        </Button>
+      </Tab>
       }
-      </Card> 
-      <Button variant="primary" style={{ marginTop: 10 }} onClick={ activate }>
-        <i className="fa fa-play"></i>&nbsp;&nbsp;Activate
-      </Button>
-    </Tab>
-    }
-  </Tabs>
+    </Tabs>
   )
 }
 

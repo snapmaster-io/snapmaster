d0f8cf7d3035d9c5b64ede7365c2cf7de7d8f3a0
Omri Gazitt <ogazitt@gmail.com>
2020-04-07 00:49:42 -0700

    cleaned up value prop styling

--- src/components/ValueProp.js
+++ src/components/ValueProp.js
@@ -4,7 +4,7 @@ const ValueProp = ({text, image, background}) => {
   const color = background === 'white' ? 'black' : 'white';
   return (
     <div style={{ backgroundColor: background, paddingTop: 50, paddingBottom: 50 }}>
-    { text &&  <h1 style={{ color: color, width: 'calc(85%)', paddingLeft: 'calc(15%)', marginBottom: 0 }}>{text}</h1> }
+    { text &&  <h2 style={{ color: color, width: 'calc(75%)', paddingLeft: 'calc(25%)', marginBottom: 0 }}>{text}</h2> }
     { image && <img style={{ paddingTop: 30, maxWidth: 'calc(80%)', maxHeight: 'calc(75%)' }} src={`/${image}`} alt={image} /> }
     </div>
   )
